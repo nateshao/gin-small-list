a3d326ffaa904f8be556c8b974cd1dd86dd968d1
nateshao <shaotongjie@gmail.com>
2021-05-05 23:56:05 +0800

    初始化前端

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// 连接数据库
 	r := gin.Default()
 
 	// 加载CSS文件
@@ -17,6 +18,15 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
+
+	// v1
+	v1Group := r.Group("v1")
+	{
+		// 待办事项
+		// 添加
+		// 查看
+		// 删除
+	}
 
 	r.Run()
 
