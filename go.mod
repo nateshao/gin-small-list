c9c1fe289b8de7b6320e1f90de390fb060a6cae2
nateshao <shaotongjie@gmail.com>
2021-05-06 22:12:46 +0800

    创建表成功

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gin-small-list
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.7.1 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.1
+	github.com/jinzhu/gorm v1.9.16
+)
--- main.go
+++ main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"net/http"
+)
+
+var (
+	DB *gorm.DB
 )
 
 // Todo model
 type Todo struct {
-	ID int `json:"id"`
-	Title string `json:"title"`
-	Status bool `json:"status"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
+
+func initMySQL() (err error) {
+	dsn := "root:123456@(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open("mysql", dsn)
+	if err != nil {
+		return
+	}
+	return DB.DB().Ping()
 }
 
 func main() {
+
 	// 连接数据库
+	err := initMySQL()
+	if err != nil {
+		panic(err)
+	}
+	defer DB.Close()        // 程序退出关闭数据库
+	DB.AutoMigrate(&Todo{}) // 创建表 todos
 	r := gin.Default()
 
 	// 加载CSS文件
@@ -31,26 +53,40 @@ func main() {
 	{
 		// 待办事项
 		// 查看所有待办事项
-		v1Group.GET("/todo", func(c *gin.Context) {
-
-		})
-		// 查看单个待办事项
-		v1Group.GET(("/todo:id", func(c *gin.Context) {
-
-		}))
-		// 添加
 		v1Group.POST("/todo", func(c *gin.Context) {
-			
-			
+			// 前端页面填写待办事项 点击提交，会发送请求到这里
+			// 1. 从请求中把数据拿出来
+			var todo Todo
+			c.BindJSON(&todo)
+			// 2. 存入数据库
+			//err = DB.Create(&todo).Error
+			//if err != nil {
+			//	panic(err)
+			//}
+			// 3. 返回响应
+			if err = DB.Create(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err})
+			} else {
+				c.JSON(http.StatusOK, todo)
+			}
 		})
-		// 修改
-		v1Group.PUT("/todo:id", func(c *gin.Context) {
-			
-		})
-		// 删除
-		v1Group.DELETE("/todo:id", func(c *gin.Context) {
-			
-		})
+		//// 查看单个待办事项
+		//v1Group.POST("/todo:id", func(c *gin.Context) {
+		//
+		//})
+		//// 添加
+		//v1Group.POST("/todo", func(c *gin.Context) {
+		//
+		//
+		//})
+		//// 修改
+		//v1Group.PUT("/todo:id", func(c *gin.Context) {
+		//
+		//})
+		//// 删除
+		//v1Group.DELETE("/todo:id", func(c *gin.Context) {
+		//
+		//})
 	}
 
 	r.Run()
